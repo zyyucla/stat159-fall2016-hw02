05230bdc21ee4107d18c1fc495dc21d63e4ffd9f
Yuyu Zhang <zyyucla@gmail.com>
2016-10-04 15:26:35 -0700

    Renaming scatterplot title

--- code/regression-script.R
+++ code/regression-script.R
@@ -10,11 +10,11 @@ regsum
 save(reg,file="regression.RData")
 
 #scatterplot with regression line
-png("../images/scatterplot.png")
+png("../images/scatterplot-tv-sales.png")
 plot(dat$TV,dat$Sales,main="Linear regression of TV ads on Sales",xlab="TV", ylab="Sales",pch = 19,col="#8e8f94")
 abline(reg,col="#bd0300",lwd=2)
 
-pdf("../images/scatterplot.pdf")
+pdf("../images/scatterplot-tv-sales.pdf")
 plot(dat$TV,dat$Sales,main="Linear regression of TV ads on Sales",xlab="TV", ylab="Sales",pch = 19,col="#8e8f94")
 abline(reg,col="#bd0300",lwd=2)
 
