90590c06623860c3a3f353d4474006a1efcc04fc
Yuyu Zhang <zyyucla@gmail.com>
2016-10-04 13:46:14 -0700

    Adding pdf file for the images

--- code/eda-script.R
+++ code/eda-script.R
@@ -10,11 +10,12 @@ sink("../data/eda-output.txt")
 summary(dat)
 sink()
 
-
+pdf("../images/histogram-tv.pdf")
 png("../images/histogram-tv.png")
 hist(dat$TV,main="Histogram of TV", xlab="TV")
 #dev.off()
 
+pdf("../images/histogram-sales.pdf")
 png("../images/histogram-sales.png")
 hist(dat$Sales,main="Histogram of Sales", xlab="Sales")
 dev.off()
