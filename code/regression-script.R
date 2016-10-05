42964e0c972d079fd63af9ab36f7ec62709e2611
Yuyu Zhang <zyyucla@gmail.com>
2016-10-04 21:43:14 -0700

    Updating regression file

--- code/regression-script.R
+++ code/regression-script.R
@@ -12,12 +12,12 @@ names(regsum)
 save(reg,file="regression.RData")
 
 #scatterplot with regression line
-#png("../images/scatterplot-tv-sales.png")
-#plot(dat$TV,dat$Sales,main="Linear regression of TV ads on Sales",xlab="TV", ylab="Sales",pch = 19,col="#8e8f94")
-#abline(reg,col="#bd0300",lwd=2)
+png("../images/scatterplot-tv-sales.png")
+plot(dat$TV,dat$Sales,main="Linear regression of TV ads on Sales",xlab="TV", ylab="Sales",pch = 19,col="#8e8f94")
+abline(reg,col="#bd0300",lwd=2)
 
-#pdf("../images/scatterplot-tv-sales.pdf")
-#plot(dat$TV,dat$Sales,main="Linear regression of TV ads on Sales",xlab="TV", ylab="Sales",pch = 19,col="#8e8f94")
-#abline(reg,col="#bd0300",lwd=2)
+pdf("../images/scatterplot-tv-sales.pdf")
+plot(dat$TV,dat$Sales,main="Linear regression of TV ads on Sales",xlab="TV", ylab="Sales",pch = 19,col="#8e8f94")
+abline(reg,col="#bd0300",lwd=2)
 
-#dev.off()
+dev.off()
